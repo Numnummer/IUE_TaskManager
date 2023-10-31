937632fd8fe672c9d87ed7f19d1063907c102975
Akhmed Mirsaitov <bignumbergames@gmail.com>
2023-10-31 12:07:42 +0300

    Определил классы

---
+++ TaskManagerModel/MyWebFramework/Router.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NtTaskWebServer.Framework
+{
+    public static class Router
+    {
+        public static async Task RouteAsync(HttpListenerContext context)
+        {
+
+        }
+    }
+}
---
+++ TaskManagerModel/MyWebFramework/WebServer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NtTaskWebServer.Framework
+{
+    public class WebServer
+    {
+        private readonly HttpListener _listener = new();
+
+
+        public async Task ListenAsync(string prefix, CancellationToken cancellationToken)
+        {
+            _listener.Prefixes.Add(prefix);
+            _listener.Start();
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var context = await _listener.GetContextAsync();
+
+            }
+            _listener.Close();
+        }
+    }
+}
