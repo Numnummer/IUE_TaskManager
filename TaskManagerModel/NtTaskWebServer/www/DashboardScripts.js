711649b3bb6340a027d0132b095175a98cad0235
Akhmed Mirsaitov <bignumbergames@gmail.com>
2023-11-09 22:17:07 +0300

    Реализовал получение данных о пользователе и их вывод на странице на фронте

--- TaskManagerModel/NtTaskWebServer/www/DashboardScripts.js
+++ TaskManagerModel/NtTaskWebServer/www/DashboardScripts.js
@@ -1,7 +1,8 @@
 function ToProfile(){
     $.get('UserProfile/UserData', function(response) {
     console.log('Успешный ответ:', response);
-    window.myObject=JSON.parse(response);
+    localStorage.setItem("UserData",response);
+    //window.myObject=JSON.parse(response);
     window.location="UserProfile";       
     }).fail(function(error) {
         console.error('Ошибка:', error);
--- TaskManagerModel/NtTaskWebServer/www/UserProfileScripts.js
+++ TaskManagerModel/NtTaskWebServer/www/UserProfileScripts.js
@@ -2,7 +2,11 @@ function ToBoard(){
     window.location="Dashboard";
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    
-    console.log(window.myObject);
+document.addEventListener('DOMContentLoaded', function() {    
+    //console.log(localStorage.getItem("UserData"));
+    var userData=JSON.parse(localStorage.getItem("UserData"));
+    console.log(userData);
+    console.log(userData.UserName);
+    document.getElementById("Name").innerText=userData.UserName;
+    document.getElementById("Email").innerText=userData.Email;
   });
